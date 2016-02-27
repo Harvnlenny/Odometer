6d912f0c20192a04ac7495e85081f6943c38ae8b
Aaron Harvey <harvnlenny@gmail.com>
2016-02-27 00:23:43 -0500

    Defined a subclass of Binder called OdometerBinder that will enable our activity to connect to the OdometerService.

---
+++ app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -0,0 +1,21 @@
+package com.hfad.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class OdometerService extends Service {
+
+    private final IBinder binder = new OdometerBinder();
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+}
